3d2d51653f5bdcc216c26c3cd5157c1738923275
xinyi le <xinyile@xinyideMacBook-Pro.local>
2019-10-24 13:37:43 -0700

    add test for discoveryRequest

--- webservice/webhook_oauth/views.py
+++ webservice/webhook_oauth/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics, permissions
 from django.contrib.auth.models import User, Group
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 from webhook_oauth.serializers import UserSerializer, GroupSerializer
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create the API views
 class UserList(generics.ListCreateAPIView):
@@ -21,7 +24,7 @@ class GroupList(generics.ListAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-
+@csrf_exempt
 def webhook(request):
     print('>>>>>WebHook')
     # TODO: https://glitch.com/edit/#!/smartthings-connector?path=index.js:34:5
@@ -31,4 +34,21 @@ def webhook(request):
         print('error')
         return redirect('https://www.google.com')
     # POST
-    print(request.POST)
+    if request.method == 'POST':
+        request_json = json.loads(request.body)
+
+        if request_json['headers']['interactionType'] == 'discoveryRequest':
+            with open('./webhook_oauth/templates/discoveryResponse.json') as json_file:
+                response_json = json.load(json_file)
+                response_json['headers'] = request_json['headers']
+                # print(response_json)
+            return JsonResponse(response_json)
+
+        elif request_json['headers']['interactionType'] == 'stateRefreshRequest':
+    
+            return ""
+        
+        elif request_json['headers']['interactionType'] == 'commandRequest':
+            
+            return ""
+
